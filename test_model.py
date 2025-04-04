6678e5b0169d2227aca3fb11d0ae150f2455b671
Maria <marriiamnii@gmail.com>
2025-04-04 23:20:48 +0800

    add tests

--- test_model.py
+++ test_model.py
@@ -1,15 +1,17 @@
 import numpy as np
 from PIL import Image
 from model import image_to_gray, ModelDiff, SubCharCNNClassifier
+from article_model import ArticleCNNClassifier
 import torch
 import os
 from sklearn.metrics import accuracy_score, precision_score, recall_score, f1_score
 
 
-name_model1 = "model1.pt"
-name_diff_model = "model_diff1.pt"
+name_model1 = "article_model2.pt"
+name_diff_model = "article_model_diff2.pt"
 
-model1 = SubCharCNNClassifier()
+# model1 = SubCharCNNClassifier()
+model1 = ArticleCNNClassifier()
 model1.load_state_dict(torch.load(name_model1, map_location=torch.device('cpu')))
 model1.eval()
 
@@ -30,60 +32,63 @@ def classifier(model1, model_diff, char_left, char_right):
         probability = torch.sigmoid(rez)
     return probability.item()
 
-# Выбираем данные для теста
-test_dir = 'test'
-class_dirs = ['0', '1']
-results = {0: [], 1: []}
 
-for class_idx, class_dir in enumerate(class_dirs):
-    class_path = os.path.join(test_dir, class_dir)
-    for img_name in os.listdir(class_path):
-        if img_name.endswith('.png'):
-            image_path = os.path.join(class_path, img_name)
-            image = Image.open(image_path)
-            image_array = np.array(image)
+def testing(test_dir, test_name):
+    class_dirs = ['0', '1']
+    results = {0: [], 1: []}
 
-            char_left = image_array[:, :40, :]
-            char_right = image_array[:, 40:, :]
+    for class_idx, class_dir in enumerate(class_dirs):
+        class_path = os.path.join(test_dir, class_dir)
+        for img_name in os.listdir(class_path):
+            if img_name.endswith('.png'):
+                image_path = os.path.join(class_path, img_name)
+                image = Image.open(image_path)
+                image_array = np.array(image)
 
-            prob = classifier(model1, diff_model, char_left, char_right)
-            pred = 1 if prob > 0.5 else 0
-            results[class_idx].append(pred)
+                char_left = image_array[:, :40, :]
+                char_right = image_array[:, 40:, :]
+
+                prob = classifier(model1, diff_model, char_left, char_right)
+                pred = 1 if prob > 0.5 else 0
+                results[class_idx].append(pred)
+
+    true_labels = []
+    predictions = []
+
+    for class_idx in [0, 1]:
+        true_labels.extend([class_idx] * len(results[class_idx]))
+        predictions.extend(results[class_idx])
+
+    accuracy = accuracy_score(true_labels, predictions)
+    precision = precision_score(true_labels, predictions)
+    recall = recall_score(true_labels, predictions)
+    f1 = f1_score(true_labels, predictions)
+
+    report = f"""
+    ======================
+    Датасет: {test_name}
+    Модель 1: {name_model1}
+    Модель Diff: {name_diff_model}
+
+    Результаты:
+    Класс 0 (разные шрифты): {results[0]}
+    Класс 1 (одинаковые шрифты): {results[1]}
+
+    Метрики:
+    Accuracy: {accuracy:.4f}
+    Precision: {precision:.4f}
+    Recall: {recall:.4f}
+    F1: {f1:.4f}
+    """
+
+    print(report)
+    return report
 
 
-true_labels = []
-predictions = []
-
-for class_idx in [0, 1]:
-    true_labels.extend([class_idx] * len(results[class_idx]))
-    predictions.extend(results[class_idx])
-
-
-accuracy = accuracy_score(true_labels, predictions)
-precision = precision_score(true_labels, predictions)
-recall = recall_score(true_labels, predictions)
-f1 = f1_score(true_labels, predictions)
-
-
+test_dirs = ['test', 'test1', 'test2']
 results_file = "test_results.txt"
 
-report = f"""
-======================
-Модель 1: {name_model1}
-Модель Diff: {name_diff_model}
-
-Результаты:
-Класс 0 (разные шрифты): {results[0]}
-Класс 1 (одинаковые шрифты): {results[1]}
-
-Метрики:
-Accuracy: {accuracy:.4f}
-Precision: {precision:.4f}
-Recall: {recall:.4f}
-F1: {f1:.4f}
-"""
-
-print(report)
 with open(results_file, 'a', encoding='utf-8') as f:
-    f.write(report)
-
+    for test_dir in test_dirs:
+        report = testing(test_dir, test_dir)
+        f.write(report)
--- train_model.py
+++ train_model.py
@@ -1,6 +1,7 @@
 import os
 
 from model import SubCharCNNClassifier, ModelDiff, CharImageDataset
+from article_model import ArticleCNNClassifier
 from torch.utils.data import DataLoader
 from torch import optim
 import torch
@@ -64,11 +65,11 @@ def train_step(batch):
 
 
 LOG_FILE = "log.train.txt"
-MODEL1_FILE = "model1.pt"
-MODEL_DIFF_FILE = "model_diff1.pt"
+MODEL1_FILE = "article_model2.pt"
+MODEL_DIFF_FILE = "article_model_diff2.pt"
 num_epochs = 20
 
-model1 = SubCharCNNClassifier().to(device)
+model1 = ArticleCNNClassifier().to(device)
 # model2 = SubCharCNNClassifier()
 model_diff = ModelDiff().to(device)
 
